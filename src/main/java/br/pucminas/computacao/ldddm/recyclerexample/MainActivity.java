1b837fe8f9f417a31fe464629f61c09ef61e2b3d
Ian Rodrigues dos Reis Paixão <36201596+IanPaixao@users.noreply.github.com>
2018-05-28 17:32:34 -0300

    Add files via upload

---
+++ src/main/java/br/pucminas/computacao/ldddm/recyclerexample/MainActivity.java
@@ -0,0 +1,38 @@
+package br.pucminas.computacao.ldddm.recyclerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+
+    MyRecyclerViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> pages = new ArrayList<>();
+        pages.add("Facebook page");
+        pages.add("Instagram page");
+        pages.add("LInkedIn page");
+        pages.add("Spotify page");
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvpages);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, pages);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+}
